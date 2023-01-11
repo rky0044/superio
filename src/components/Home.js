8254c1f0f72f41597b5a786ee32905c462d04374
gdpython01 <gdpython01@gmail.com>
2023-01-11 05:35:38 +0530

    banner add page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,16 @@
 import * as React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import BannerSection from './BannerSection';
 
 const Home = () => {
     return (
         <>
-            <Header />
-            <Footer />
+         <div className="page-wrapper">
+                <Header />
+                <BannerSection />
+                <Footer />
+            </div>
         </>
     );
 };
